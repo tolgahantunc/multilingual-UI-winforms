4bf07e52bb900ab41dc26d9e1ac9b86191817e32
Tolgahan Tunç <tunctolgahan@gmail.com>
2020-09-15 01:13:28 +0300

    project initialization

---
+++ WindowsFormsApp1/WelcomePage.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class WelcomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(12, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = global::WindowsFormsApp1.Resources.lang_tr.LOC_Okay;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(13, 156);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = global::WindowsFormsApp1.Resources.lang_tr.LOC_Cancel;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Türkçe",
+            "English"});
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(173, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 194);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 32);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 43);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 32);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(111, 43);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 32);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // WelcomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 265);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "WelcomePage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WelcomePage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
---
+++ WindowsFormsApp1/WelcomePage.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class WelcomePage : Form
+    {
+        public static ResourceManager rm;
+
+        private Int32 m_Counter = 0;
+        private String m_AppLanguage = String.Empty;
+        private static String m_CurrentAppLanguage = Properties.Settings.Default.ApplicationLanguage;
+        public WelcomePage()
+        {
+            InitializeComponent();
+
+            //TODO get the system's Culture info and apply it to the program to change some default values used by the code, ie MessageBoxButtons.YesNo buttons.
+
+            GetCurrentApplicationLanguage();
+
+            DialogResult dr = MessageBox.Show(rm.GetString("LOC_UpdateUIObjectsText"), rm.GetString("LOC_Warning"), MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+                AssignTextToUIObjects();
+        }
+
+        private void GetCurrentApplicationLanguage()
+        {
+            switch (m_CurrentAppLanguage)
+            {
+                case "TR":
+                    rm = new ResourceManager("WindowsFormsApp1.Resources.lang_tr", Assembly.GetExecutingAssembly());
+                    break;
+                case "EN":
+                    rm = new ResourceManager("WindowsFormsApp1.Resources.lang_en", Assembly.GetExecutingAssembly());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = (ComboBox)sender;
+            switch (cb.Text)
+            {
+                case "Türkçe":
+                    rm = new ResourceManager("WindowsFormsApp1.Resources.lang_tr", Assembly.GetExecutingAssembly());
+                    m_AppLanguage = "TR";
+                    break;
+                case "English":
+                    rm = new ResourceManager("WindowsFormsApp1.Resources.lang_en", Assembly.GetExecutingAssembly());
+                    m_AppLanguage = "EN";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void AssignTextToUIObjects()
+        {
+            button1.Text = rm.GetString("LOC_Okay");
+            button2.Text = rm.GetString("LOC_Cancel");
+            button3.Text = rm.GetString("LOC_Increase");
+            button4.Text = rm.GetString("LOC_Save");
+            button5.Text = rm.GetString("LOC_Refresh");
+            label1.Text = String.Format(rm.GetString("LOC_RemainingDays"), m_Counter, "çok");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            m_Counter++;
+            label1.Text = String.Format(rm.GetString("LOC_RemainingDays"), m_Counter, "very");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //if app lang combobox selection is empty, show message and return
+            if(String.IsNullOrEmpty(m_AppLanguage))
+            {
+                string msg = rm.GetString("LOC_AppLanguageNotSelected");
+                MessageBox.Show(msg);
+                return;
+            }
+
+            Properties.Settings.Default.ApplicationLanguage = m_AppLanguage;
+            Properties.Settings.Default.Save();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            AssignTextToUIObjects();
+        }
+    }
+}
+
+
